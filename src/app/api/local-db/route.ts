754e752823cf2e08ea6e94236b4c447211691ae7
刘胜 <282664560@qq.com>
2023-12-11 15:16:10 +0800

    fix: local-db to network file

--- src/app/api/local-db/route.ts
+++ src/app/api/local-db/route.ts
@@ -1,5 +1,7 @@
+import { cosUpload } from '@/helper/cos'
 import { success } from '@/helper/response'
 import { NextRequest } from 'next/server'
+import path from 'path'
 import { getQuery } from 'utils/api-route'
 import { db } from 'utils/db'
 
@@ -7,11 +9,39 @@ export async function GET(req: NextRequest) {
   const { mainKey, minorKey } = getQuery(req)
   const data = (await db.getObjectDefault(`.${mainKey}.${minorKey}`)) || {}
 
-  console.log('🚀 ~ file: route.ts:10 ~ GET ~ data:', data)
-  return success(data)
+  // const file = path.join(process.cwd(), '', 'local-db.json')
+  // const stringified = readFileSync(file, 'utf8')
+
+  const file = path.join('/tmp/local-db.json')
+  await cosUpload({
+    filepath: file,
+    newFilename: 'local-db',
+    suffix: 'json'
+  })
+
+  // fetch 读取网络文件 https://cdn-1257429552.cos.ap-guangzhou.myqcloud.com/json/local-db.json
+  const response = await fetch(
+    'https://cdn-1257429552.cos.ap-guangzhou.myqcloud.com/json/local-db.json'
+  )
+  const stringified = await response.text()
+  const fileJsonObj = JSON.parse(stringified) || {}
+  console.log('fileJsonObj =>', fileJsonObj)
+
+  return success(fileJsonObj)
+  // return success(data)
 }
 
 export async function POST(req: NextRequest) {
   const query = await req.json()
+  const data = (await db.getObjectDefault(`.mallWithAccount.11707`)) || {}
+  const file = path.join('/tmp/local-db.json')
+  await cosUpload({
+    filepath: file,
+    newFilename: 'local-db',
+    suffix: 'json'
+  })
+  // const fileJson = readFileSync(file, 'utf8')
+  // const fileJsonObj = JSON.parse(fileJson) || {}
+
   return success(query)
 }
---
+++ src/helper/cos.ts
@@ -0,0 +1,48 @@
+import COS from 'cos-nodejs-sdk-v5'
+import fs from 'fs'
+
+// cos 前缀
+const cosPrefix = 'https://cdn-1257429552.cos.ap-guangzhou.myqcloud.com'
+
+const cos = new COS({
+  SecretId: 'AKIDd9UcVDi16xZ0MYdXN3XotpY5TdYDcnjP',
+  SecretKey: 'UigOq3zf5GExPeiZAVL1UihU4PVMzOXR'
+})
+
+const bucket = {
+  Bucket: 'cdn-1257429552',
+  Region: 'ap-guangzhou',
+  Key: 'json',
+  cdn: '',
+  path: '/'
+}
+
+export const cosUpload = async (info: any) => {
+  return new Promise((resolve, reject) => {
+    const { filepath, newFilename, suffix } = info
+    const objectKey = `${bucket.Key + bucket.path + newFilename}.${suffix}`
+    cos.putObject(
+      {
+        Bucket: bucket.Bucket,
+        Region: bucket.Region,
+        Key: objectKey,
+        StorageClass: 'STANDARD',
+        Body: fs.createReadStream(filepath),
+        onProgress: (progressData: any) => {
+          const { percent } = progressData
+          console.log(`上传进度 --- ${percent}%`)
+        }
+      },
+      (err: any, data: any) => {
+        if (err) {
+          reject(err)
+        } else {
+          resolve({
+            url: `${cosPrefix}/${objectKey}`,
+            ...info
+          })
+        }
+      }
+    )
+  })
+}
