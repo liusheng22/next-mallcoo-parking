7172109b170b316bcca6fd9c850949d1bce84f78
刘胜 <282664560@qq.com>
2024-01-06 20:26:30 +0800

    fix: add log output

--- src/app/api/mallcoo/info/route.ts
+++ src/app/api/mallcoo/info/route.ts
@@ -1,3 +1,4 @@
+import { fetchGetParkFeeInit } from '@/helper/payment/park-fee'
 import { success } from '@/helper/response'
 import { NextRequest } from 'next/server'
 import { getQuery } from 'utils/api-route'
@@ -5,8 +6,18 @@ import { getQuery } from 'utils/api-route'
 export async function GET(req: NextRequest) {
   const query = getQuery(req)
   console.log('getMallcooApi query =>', query)
+  const { uid, plateNo, mallId, parkId } = query || {}
 
-  return success(query)
+  // 查询待缴费信息，根据 EntryTime 判断是否车辆在场
+  const data = await fetchGetParkFeeInit({
+    uid,
+    plateNo,
+    mallId,
+    parkId
+  })
+  console.log('查询停车信息 =>', data)
+
+  return success(data)
 }
 
 export async function POST() {
