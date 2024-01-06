2cfab0025df32935023a4dc0f88581de9af584c2
刘胜 <282664560@qq.com>
2024-01-06 21:41:58 +0800

    fix: add log output

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,14 @@
-import { fetcher } from 'app/composables/use-fetcher'
+// import { fetcher } from 'app/composables/use-fetcher'
 import type { NextRequest } from 'next/server'
 import { NextResponse } from 'next/server'
 
-setTimeout(() => {
-  fetcher({
-    url: '/api/task',
-    method: 'POST',
-    data: {}
-  })
-}, 1000)
+// setTimeout(() => {
+//   fetcher({
+//     url: '/api/task',
+//     method: 'POST',
+//     data: {}
+//   })
+// }, 1000)
 
 export function middleware(req: NextRequest) {
   const { pathname } = req.nextUrl
