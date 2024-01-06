88a743e0ae2f961d6add53fae6c76902451fdec1
刘胜 <282664560@qq.com>
2024-01-06 22:13:56 +0800

    fix: add log output

--- src/helper/payment/park-fee.ts
+++ src/helper/payment/park-fee.ts
@@ -7,13 +7,26 @@ export const fetchGetParkFeeInit: any = async (query: PayInfo) => {
   const { uid, mallId, plateNo, parkId } = query
   const url = 'https://m.mallcoo.cn/api/park/ParkFee/GetParkFeeInit'
 
+  // const params = {
+  //   UID: uid,
+  //   PlateNo: plateNo,
+  //   MallID: mallId,
+  //   ParkID: parkId,
+  //   Barcode: '',
+  //   IsVerifyWaitPay: true
+  // }
+
   const params = {
-    UID: uid,
-    PlateNo: plateNo,
-    MallID: mallId,
-    ParkID: parkId,
+    UID: '167476415',
+    MallID: 11707,
+    ParkID: 985,
+    PlateNo: '川F5N0S2',
     Barcode: '',
-    IsVerifyWaitPay: true
+    IsVerifyWaitPay: true,
+    UUID: '7a9e0ec5a90347ff8ce66415a97925bb',
+    Header: {
+      Token: 'QgHHpiWlXk-5-ICySehwQARYMAX9w_10,16706'
+    }
   }
   console.log('fetchGetParkFeeInit params =>', params)
 
